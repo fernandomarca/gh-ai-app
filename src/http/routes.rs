b1cd6e4faabeffb6d1ad30c8353af07f0ac611a5
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2024-12-09 12:38:26 -0300

    allow origins

--- src/http/routes.rs
+++ src/http/routes.rs
@@ -2,13 +2,19 @@ use super::infra::health::health_routes;
 use super::ollama::config_routes::ollama_routes;
 use axum::Router;
 use tower_http::cors::{Any, CorsLayer};
+
 pub fn app_routes() -> Router {
+    let origins = [
+        "https://dev.oclm.com.br".parse().unwrap(),
+        "https://oclm.com.br".parse().unwrap(),
+        "http://localhost:8080".parse().unwrap(),
+    ];
     Router::new()
         .nest("/", health_routes())
         .nest("/api", ollama_routes())
         .layer(
             CorsLayer::new()
-                .allow_origin(Any)
+                .allow_origin(origins)
                 .allow_headers(Any)
                 .allow_methods(Any),
         )
