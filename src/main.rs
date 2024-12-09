dd948743322822606f96c87096994100239f7dd1
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2024-12-09 14:00:05 -0300

    config nginx

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ struct Ports {
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8081").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     println!("Server running on port {}", listener.local_addr().unwrap());
     axum::serve(listener, app_routes()).await.unwrap();
